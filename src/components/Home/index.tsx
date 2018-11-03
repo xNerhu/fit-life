982d1bd5c721911ffd12891d7069e4c31db1f756
unknown <xnerhu@gmail.com>
2018-11-03 16:02:24 +0100

    Improve start design

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -3,114 +3,31 @@ import * as React from 'react';
 import { PRIMARY_COLOR } from '~/constants';
 import Button from '../Button';
 import Section from '../Section';
-import { Start, Footer } from './styles';
+import { Start, StartContainer, StartTitle, StartSubtitle } from './styles';
 
 export default class Home extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Start.Root>
-          <Start.Title>Projekt Fit Life</Start.Title>
-          <Start.Subtitle>Zmień swoje życie na lepsze</Start.Subtitle>
-          <Button
-            variant="outlined"
-            borderColor={PRIMARY_COLOR}
-            foreground={PRIMARY_COLOR}
-            text="WEŹ UDZIAŁ"
-            style={{
-              marginTop: 32,
-            }}
-          />
-        </Start.Root>
-        <Section
-          title="O projekcie"
-          vertical={false}
-          style={{
-            marginTop: 64,
-            paddingBottom: 48,
-            borderBottom: '1px solid rgba(0, 0, 0, 0.08)',
-          }}
-        >
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
-          facere labore doloremque sed dolor ipsum, ex cumque aperiam, nihil
-          voluptate voluptatibus inventore recusandae! Optio et sed expedita
-          eius placeat nesciunt? Lorem ipsum dolor sit, amet consectetur
-          adipisicing elit. Quia eos, quae dolores earum officia optio quibusdam
-          incidunt ipsam vel deserunt, voluptates, corporis placeat culpa
-          delectus. Nobis, vitae deleniti. Est, ipsam? Lorem ipsum dolor sit,
-          amet consectetur adipisicing elit. Amet, earum quibusdam officia
-          assumenda ipsam temporibus quos tenetur aut inventore, exercitationem
-          repellendus dolores. Rerum, fugiat iusto. Necessitatibus illo
-          quibusdam totam quae.
-        </Section>
-        <Section title="O problemie i sposobach jego ograniczenia">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
-          facere labore doloremque sed dolor ipsum, ex cumque aperiam, nihil
-          voluptate voluptatibus inventore recusandae! Optio et sed expedita
-          eius placeat nesciunt? Lorem ipsum dolor sit, amet consectetur
-          adipisicing elit. Quia eos, quae dolores earum officia optio quibusdam
-          incidunt ipsam vel deserunt, voluptates, corporis placeat culpa
-          delectus. Nobis, vitae deleniti. Est, ipsam? Lorem ipsum dolor sit,
-          amet consectetur adipisicing elit. Amet, earum quibusdam officia
-          assumenda ipsam temporibus quos tenetur aut inventore, exercitationem
-          repellendus dolores. Rerum, fugiat iusto. Necessitatibus illo
-          quibusdam totam quae.
-        </Section>
-        <Section title="Wydarzenia">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
-          facere labore doloremque sed dolor ipsum, ex cumque aperiam, nihil
-          voluptate voluptatibus inventore recusandae! Optio et sed expedita
-          eius placeat nesciunt? Lorem ipsum dolor sit, amet consectetur
-          adipisicing elit. Quia eos, quae dolores earum officia optio quibusdam
-          incidunt ipsam vel deserunt, voluptates, corporis placeat culpa
-          delectus. Nobis, vitae deleniti. Est, ipsam? Lorem ipsum dolor sit,
-          amet consectetur adipisicing elit. Amet, earum quibusdam officia
-          assumenda ipsam temporibus quos tenetur aut inventore, exercitationem
-          repellendus dolores. Rerum, fugiat iusto. Necessitatibus illo
-          quibusdam totam quae.
-        </Section>
-        <Section title="Blog">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
-          facere labore doloremque sed dolor ipsum, ex cumque aperiam, nihil
-          voluptate voluptatibus inventore recusandae! Optio et sed expedita
-          eius placeat nesciunt? Lorem ipsum dolor sit, amet consectetur
-          adipisicing elit. Quia eos, quae dolores earum officia optio quibusdam
-          incidunt ipsam vel deserunt, voluptates, corporis placeat culpa
-          delectus. Nobis, vitae deleniti. Est, ipsam? Lorem ipsum dolor sit,
-          amet consectetur adipisicing elit. Amet, earum quibusdam officia
-          assumenda ipsam temporibus quos tenetur aut inventore, exercitationem
-          repellendus dolores. Rerum, fugiat iusto. Necessitatibus illo
-          quibusdam totam quae.
-        </Section>
-        <Section title="Kontakt">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
-          facere labore doloremque sed dolor ipsum, ex cumque aperiam, nihil
-          voluptate voluptatibus inventore recusandae! Optio et sed expedita
-          eius placeat nesciunt? Lorem ipsum dolor sit, amet consectetur
-          adipisicing elit. Quia eos, quae dolores earum officia optio quibusdam
-          incidunt ipsam vel deserunt, voluptates, corporis placeat culpa
-          delectus. Nobis, vitae deleniti. Est, ipsam? Lorem ipsum dolor sit,
-          amet consectetur adipisicing elit. Amet, earum quibusdam officia
-          assumenda ipsam temporibus quos tenetur aut inventore, exercitationem
-          repellendus dolores. Rerum, fugiat iusto. Necessitatibus illo
-          quibusdam totam quae.
-        </Section>
-        <Footer.Root>
-          <Footer.Container>
-            <Footer.Column>
-              <Footer.Title>Organizatorzy</Footer.Title>
-            </Footer.Column>
-            <Footer.Column>
-              <Footer.Title>Kontakt</Footer.Title>
-            </Footer.Column>
-            <Footer.Column>
-              <Footer.Title>Bądź na bieżąco</Footer.Title>
-            </Footer.Column>
-            <Footer.Column>
-              <Footer.Title>Projekty</Footer.Title>
-            </Footer.Column>
-          </Footer.Container>
-        </Footer.Root>
+        <Start>
+          <StartContainer>
+            <StartTitle>Projekt Fit Life</StartTitle>
+            <StartSubtitle>Zmień swoje życie na lepsze</StartSubtitle>
+            <Button
+              variant="outlined"
+              borderColor={PRIMARY_COLOR}
+              foreground={PRIMARY_COLOR}
+              text="WEŹ UDZIAŁ"
+              style={{
+                position: 'absolute',
+                left: '50%',
+                bottom: '-96px',
+                transform: 'translateX(-50%)',
+              }}
+            />
+          </StartContainer>
+        </Start>
+        xd
       </React.Fragment>
     );
   }
--- src/components/Home/styles.ts
+++ src/components/Home/styles.ts
@@ -1,2 +1,50 @@
-export * from './start';
-export * from './footer';
+import styled from 'styled-components';
+
+import { images } from '~/constants';
+import { edoRegular, h5 } from '~/mixins';
+
+export const Start = styled.div`
+  width: 100%;
+  min-height: 75vh;
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+
+  &::before {
+    content: '';
+    display: block;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    transform: scale(1.1);
+    filter: blur(16px);
+    background-image: url(${images.header});
+  }
+`;
+
+export const StartContainer = styled.div`
+  position: relative;
+  text-align: center;
+`;
+
+export const StartTitle = styled.div`
+  font-size: 60px;
+  color: #fff;
+
+  ${edoRegular()};
+`;
+
+export const StartSubtitle = styled.div`
+  color: rgba(255, 255, 255, 0.8);
+  margin-top: 12px;
+
+  ${h5()};
+`;
